b1fae71dffe3f048324f07e792c98a95cd72f435
unknown <akun.satriyonugroho2@gmail.com>
2022-08-18 13:29:57 +0800

    nomer 4

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.main')
+
+
+@section('container')
+
+    <h1>
+        Halaman About
+    </h1>
+    <h3>
+        Satriyo Nugroho
+    </h3>
+    <p>
+        email
+    </p>
+
+@endsection
+    
