7dbf213e11a3bc98fb3325bb3383bb1598f56d62
SimpleTechDev1 <programmingkalsel.error@gmail.com>
2022-08-20 11:54:24 +0800

    nomer 9

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Post;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,5 +18,48 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+
+        User::create([
+            'name' => 'Satriyo Nugroho',
+            'email' => 'satriyonugroho2@gmail.com',
+            'password' => bcrypt('12345'),
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming',
+        ]);
+
+        Category::create([
+            'name' => 'Web Design',
+            'slug' => 'web-design',
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi sapiente hic eaque eius, nisi dolore. Ad, fuga optio dolores quis repudiandae voluptate accusamus quo minima commodi doloribus iste aperiam adipisci nesciunt recusandae accusantium voluptatum ea iure architecto praesentium vitae.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi sapiente hic eaque eius, nisi dolore. Ad, fuga optio dolores quis repudiandae voluptate accusamus quo minima commodi doloribus iste aperiam adipisci nesciunt recusandae accusantium voluptatum ea iure architecto praesentium vitae. Distinctio debitis ullam velit dignissimos voluptatum, recusandae eligendi? Fugit et, non quia nostrum eos, deserunt quisquam accusamus dolor ipsum blanditiis impedit rem, reiciendis facilis nisi cumque harum assumenda laboriosam dolorum dignissimos! Explicabo magni, expedita tempore debitis eum eveniet pariatur odio maxime, hic nobis quis! Fugiat facere rerum, praesentium illum aspernatur pariatur. Culpa sit ad exercitationem aut quibusdam autem similique. Consequuntur, vero.',
+            'category_id' => 1,
+            'user_id' => 1,
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi sapiente hic eaque eius, nisi dolore. Ad, fuga optio dolores quis repudiandae voluptate accusamus quo minima commodi doloribus iste aperiam adipisci nesciunt recusandae accusantium voluptatum ea iure architecto praesentium vitae.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi sapiente hic eaque eius, nisi dolore. Ad, fuga optio dolores quis repudiandae voluptate accusamus quo minima commodi doloribus iste aperiam adipisci nesciunt recusandae accusantium voluptatum ea iure architecto praesentium vitae. Distinctio debitis ullam velit dignissimos voluptatum, recusandae eligendi? Fugit et, non quia nostrum eos, deserunt quisquam accusamus dolor ipsum blanditiis impedit rem, reiciendis facilis nisi cumque harum assumenda laboriosam dolorum dignissimos! Explicabo magni, expedita tempore debitis eum eveniet pariatur odio maxime, hic nobis quis! Fugiat facere rerum, praesentium illum aspernatur pariatur. Culpa sit ad exercitationem aut quibusdam autem similique. Consequuntur, vero.',
+            'category_id' => 1,
+            'user_id' => 1,
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi sapiente hic eaque eius, nisi dolore. Ad, fuga optio dolores quis repudiandae voluptate accusamus quo minima commodi doloribus iste aperiam adipisci nesciunt recusandae accusantium voluptatum ea iure architecto praesentium vitae.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi sapiente hic eaque eius, nisi dolore. Ad, fuga optio dolores quis repudiandae voluptate accusamus quo minima commodi doloribus iste aperiam adipisci nesciunt recusandae accusantium voluptatum ea iure architecto praesentium vitae. Distinctio debitis ullam velit dignissimos voluptatum, recusandae eligendi? Fugit et, non quia nostrum eos, deserunt quisquam accusamus dolor ipsum blanditiis impedit rem, reiciendis facilis nisi cumque harum assumenda laboriosam dolorum dignissimos! Explicabo magni, expedita tempore debitis eum eveniet pariatur odio maxime, hic nobis quis! Fugiat facere rerum, praesentium illum aspernatur pariatur. Culpa sit ad exercitationem aut quibusdam autem similique. Consequuntur, vero.',
+            'category_id' => 2,
+            'user_id' => 1,
+        ]);
     }
 }
--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -8,7 +8,7 @@
     @foreach($posts as $post)
 
     <article class="mb-5">
-    <h2><a href="/post/{{ $post->slug}}">{{ $post->title }} </a></h2>
+    <h2><a href="/post/{{ $post->slug}}" class="text-decoration-none">{{ $post->title }} </a></h2>
     <h5>{{ $post->author }}</h5>
     <p>
     {{ $post->excerpt }}
