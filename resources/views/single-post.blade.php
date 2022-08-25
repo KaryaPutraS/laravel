e014e5a7d390c9d65f72b3e6db3d2e58ffd12ab1
SimpleTechDev1 <programmingkalsel.error@gmail.com>
2022-08-25 19:20:58 +0800

    tes

--- resources/views/single-post.blade.php
+++ resources/views/single-post.blade.php
@@ -20,7 +20,7 @@
                 <article class="my-3 fs-5">
                     {!! $post->body !!}
                 </article>
-                <a href="/post" class="text-decoration-none">BACK</a>
+                <a href="/post" class="text-decoration-none">BACK TO POST</a>
                 
             </div>
         </div>
