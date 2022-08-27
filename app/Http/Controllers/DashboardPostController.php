98e461f943c4510100916763697c46dbd8d7fd38
KaryaPutraS <akun.satriyonugroho2@gmail.com>
2022-08-27 14:41:47 +0800

    nomer 20

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -51,7 +51,7 @@ class DashboardPostController extends Controller
         ]);
 
         $validateData['user_id'] = auth()->user()->id;
-        $validateData['excerpt'] = Str::limit($request->body, 200);
+        $validateData['excerpt'] = Str::limit(strip_tags($request->body), 200);
 
         Post::create($validateData);
 
@@ -79,7 +79,10 @@ class DashboardPostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        return view('dashboard.posts.edit', [
+            'post' => $post,
+            'categories' => Category::all()
+        ]);
     }
 
     /**
@@ -91,7 +94,26 @@ class DashboardPostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        //
+        $rulePost = [
+            'title' => 'required|max:255',
+            'category_id' => 'required',
+            'body' => 'required',
+
+        ];
+
+        if ($request->slug != $post->slug) {
+            $rulePost['slug'] = 'required|unique:posts';
+        }
+
+        $validateData = $request->validate($rulePost);
+
+        $validateData['user_id'] = auth()->user()->id;
+        $validateData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+
+        Post::where('id', $post->id)
+            ->update($validateData);
+
+        return redirect('/dashboard/posts')->with('success', 'Post has been Updated!');
     }
 
     /**
@@ -102,7 +124,9 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        Post::destroy($post->id);
+
+        return redirect('/dashboard/posts')->with('success', 'Post has been deleted!');
     }
 
     public function checkSlug(Request $request)
--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -7,12 +7,12 @@
     </div>
 
     @if (session()->has('success'))
-      <div class="aler alert-success">
+      <div class="aler alert-success col-lg-8">
         {{ session('success') }}
       </div>
     @endif
 
-    <div class="table-responsive">
+    <div class="table-responsive col-lg-8">
       <a href="/dashboard/posts/create" class="btn btn-primary mt-3">Create New Post</a>
         <table class="table table-striped table-sm">
           <thead>
@@ -34,11 +34,18 @@
                 <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info">
                     <span data-feather="eye" ></span>
                 </a>
-                <a href="/dashboard/posts/{{ $post->id }}" class="badge bg-warning">
+                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning">
                     <span data-feather="edit" ></span>
                 </a>
-                <a href="/dashboard/posts/{{ $post->id }}" class="badge bg-danger">
+                <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline">
+                  @method('delete')
+                  @csrf
+                  <button class="badge bg-danger border-0" onclick="return confirm('Are You Sure?')">
                     <span data-feather="x-circle"></span>
+                  </button>
+                </form>
+
+                    
                 </a>
               </td>
             </tr>
